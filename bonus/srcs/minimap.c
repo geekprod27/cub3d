29fb36925d11d593d75e813df45ca5491ede5521
llepiney <llepiney@student.42.fr>
2022-11-23 17:37:19 +0100

    minimap crash

--- bonus/srcs/minimap.c
+++ bonus/srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: llepiney <llepiney@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:52:03 by llepiney          #+#    #+#             */
-/*   Updated: 2022/11/22 17:39:46 by llepiney         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:35:16 by llepiney         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,57 +19,53 @@ void	minimap(t_data *d)
 	int		y;
 	int		i;
 	int		j;
-	int		tmp;
 	int		coords;
 
 
 	x = 9;
-	while (x < 60)
+	//printf("posx %f, posy %f\n", d->posx, d->posy);
+	i = d->posy - 5;
+	while (x < 110)
 	{
+		j = d->posx - 5;
 		y = 9;
-		while (y < 60)
+		while (y < 110)
 		{
-			tmp = 0;
-			while (y < 60 && tmp < 10)
+			coords = x * d->mlx->line_size + y * (d->mlx->bitperpixel / 8);
+			if (i < 0 || j < 0)
 			{
-				i = d->posx - 2;
-				j =  d->posy - 2;
-				coords = x * d->mlx->line_size + y * (d->mlx->bitperpixel / 8);
-				if (i < 0 || j < 0)
-				{
-					d->mlx->mlx_imgadr[coords] = 0;
-					d->mlx->mlx_imgadr[coords + 1] = 0;
-					d->mlx->mlx_imgadr[coords + 2] = 0;
-					d->mlx->mlx_imgadr[coords + 3] = 1;
-				}
-				else if (i == d->posy && j == d->posx)
-				{
-					d->mlx->mlx_imgadr[coords] = 0;
-					d->mlx->mlx_imgadr[coords + 1] = 0;
-					d->mlx->mlx_imgadr[coords + 2] = 153;
-					d->mlx->mlx_imgadr[coords + 3] = 1;
-				}
-				else if (d->map[x / 10][y / 10] == '1')
-				{
-					d->mlx->mlx_imgadr[coords] = 255;
-					d->mlx->mlx_imgadr[coords + 1] = 153;
-					d->mlx->mlx_imgadr[coords + 2] = 153;
-					d->mlx->mlx_imgadr[coords + 3] = 1;
-				}
-				else if (d->map[x / 10][y / 10] == '0')
-				{
-					d->mlx->mlx_imgadr[coords] = 255;
-					d->mlx->mlx_imgadr[coords + 1] = 204;
-					d->mlx->mlx_imgadr[coords + 2] = 153;
-					d->mlx->mlx_imgadr[coords + 3] = 1;
-				}
-				y++;
-				tmp++;
-				if (tmp == 10)
-					tmp = 0;
+				continue;
 			}
+			else if (i == d->posy && j == d->posx)
+			{
+				d->mlx->mlx_imgadr[coords] = 0;
+				d->mlx->mlx_imgadr[coords + 1] = 0;
+				d->mlx->mlx_imgadr[coords + 2] = 153;
+				d->mlx->mlx_imgadr[coords + 3] = 1;
+			}
+			else if (d->map[i][j] == '1')
+			{
+				//printf("1:%c\n", d->map[(x + 1) / 10][(y + 1) / 10]);
+				d->mlx->mlx_imgadr[coords] = 255;
+				d->mlx->mlx_imgadr[coords + 1] = 153;
+				d->mlx->mlx_imgadr[coords + 2] = 153;
+				d->mlx->mlx_imgadr[coords + 3] = 1;
+			}
+			else if (d->map[i][j] == '0')
+			{
+				//printf("0:%c\n", d->map[(x + 1) / 10][(y + 1) / 10]);
+				d->mlx->mlx_imgadr[coords] = 255;
+				d->mlx->mlx_imgadr[coords + 1] = 204;
+				d->mlx->mlx_imgadr[coords + 2] = 153;
+				d->mlx->mlx_imgadr[coords + 3] = 1;
+			}
+			printf("i:%d, j:%d\n", i, j);
+			if ((y + 1) % 10 == 0)
+				j++;
 			y++;
 		}
+		if ((x + 1) % 10 == 0)
+			i++;
 		x++;
 	}
 }
