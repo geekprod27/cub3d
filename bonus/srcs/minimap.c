294870bc64d124f4af00b4aad807978ccf24d083
llepiney <llepiney@student.42.fr>
2022-11-23 18:11:49 +0100

    minimap debug

--- bonus/srcs/minimap.c
+++ bonus/srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: llepiney <llepiney@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:52:03 by llepiney          #+#    #+#             */
-/*   Updated: 2022/11/23 18:03:55 by llepiney         ###   ########.fr       */
+/*   Updated: 2022/11/23 18:11:20 by llepiney         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,33 +24,37 @@ void	minimap(t_data *d)
 
 	x = 9;
 	//printf("posx %f, posy %f\n", d->posx, d->posy);
-	i = d->posy - 5;
-	while (x < 110 && i < d->xmax && i > 0)
+	i = (int)d->posy - 5;
+	while (x < 110 && i < d->xmax)
 	{
-		j = d->posx - 5;
+		j = (int)d->posx - 5;
 		y = 9;
-		while (y < 110 && j < d->ymax && j > 0)
+		while (y < 110 && j < d->ymax)
 		{
 			coords = x * d->mlx->line_size + y * (d->mlx->bitperpixel / 8);
 			if (i < 0 || i >= d->xmax || j < 0 || j >= d->ymax)
 			{
+				if (i < 0)
+					i++;
+				if (j < 0)
+					j++;
 				continue;
 			}
-			else if (d->map[i][j] == '0' || d->map[i][j] == d->spawn)
+			if (d->map[i][j] == '0' || d->map[i][j] == d->spawn)
 			{
 				d->mlx->mlx_imgadr[coords] = 255;
 				d->mlx->mlx_imgadr[coords + 1] = 204;
 				d->mlx->mlx_imgadr[coords + 2] = 153;
 				d->mlx->mlx_imgadr[coords + 3] = 1;
 			}
-			else if (i == d->posy && j == d->posx)
+			if (i == (int)d->posy && j == (int)d->posx)
 			{
 				d->mlx->mlx_imgadr[coords] = 0;
 				d->mlx->mlx_imgadr[coords + 1] = 0;
 				d->mlx->mlx_imgadr[coords + 2] = 153;
 				d->mlx->mlx_imgadr[coords + 3] = 1;
 			}
-			else if (d->map[i][j] == '1')
+			if (d->map[i][j] == '1')
 			{
 				d->mlx->mlx_imgadr[coords] = 255;
 				d->mlx->mlx_imgadr[coords + 1] = 153;
