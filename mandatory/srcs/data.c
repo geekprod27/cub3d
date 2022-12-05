c3893643bd9239696db207c9802be2fcaba6aa22
Nathan FELSEMBERG <nfelsemb@e2r9p7.clusters.42paris.fr>
2022-12-05 15:57:26 +0100

    fix check rgb

--- mandatory/srcs/data.c
+++ mandatory/srcs/data.c
@@ -6,7 +6,7 @@
 /*   By: nfelsemb <nfelsemb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 11:51:25 by nfelsemb          #+#    #+#             */
-/*   Updated: 2022/11/23 13:56:01 by nfelsemb         ###   ########.fr       */
+/*   Updated: 2022/12/05 15:55:36 by nfelsemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,11 @@ void	checkvir(char *line, int i, t_data *data, int fd)
 	while (line[i])
 	{
 		if (line[i] == ',')
+		{
 			contvir++;
+			if (!ft_isdigit(line[i - 1] || !ft_isdigit(line[i + 1])))
+				errorrgb(data, line - 1, fd);
+		}
 		if (contvir > 2)
 			errorrgb(data, line - 1, fd);
 		i++;
@@ -53,7 +57,7 @@ void	checkchar(char *line, int i, t_data *data, int fd)
 	}
 }
 
-int	skipspace(char *line)
+int	skipspaced(char *line)
 {
 	int	i;
 
@@ -65,19 +69,33 @@ int	skipspace(char *line)
 
 void	get_trgb(char *line, t_data	*data, char l, int fd)
 {
-	// int		r;
-	// int		g;
-	// int		b;
+	int		r;
+	int		g;
+	int		b;
 	int		i;
 
-	i = skipspace(line);
+	i = skipspaced(line);
 	checkchar(line, i, data, fd);
 	checkvir(line, i, data, fd);
-	(void) l;
-	// if (l == 'C')
-	// 	savedatac(data, r, g, b);
-	// else
-	// 	savedataf(data, r, g, b);
+	r = ft_atoi(line + i);
+	while (line[i] && line[i] != ',')
+		i++;
+	if (line[i] && line[i + 1])
+		i++;
+	else
+		errorrgb(data, line - 1, fd);
+	g = ft_atoi(line + i);
+	while (line[i] && line[i] != ',')
+		i++;
+	if (line[i] && line[i + 1])
+		i++;
+	else
+		errorrgb(data, line - 1, fd);
+	b = ft_atoi(line + i);
+	if (l == 'C')
+		savedatac(data, r, g, b);
+	else
+		savedataf(data, r, g, b);
 }
 
 char	**add(char **map, int i, char *line)
